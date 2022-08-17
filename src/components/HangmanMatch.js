bfb09215d68df79619b7a127eafa0f8cc2fcbea5
orozCoding <orozcoding@gmail.com>
2022-08-17 07:51:24 -0400

    Add page form to start game

--- src/components/HangmanMatch.js
+++ src/components/HangmanMatch.js
@@ -1,131 +1,158 @@
 import React, { useEffect, useState } from "react";
 
 const HangmanMatch = (props) => {
-  const { word, limit } = props;
+  const { word, limit, hint, setReady } = props;
   const [tries, setTries] = useState(0);
   const [board, setBoard] = useState([]);
   const [completed, setCompleted] = useState(false);
   const [hp, setHp] = useState(limit);
   const [used, setUsed] = useState([]);
+  const [error, setError] = useState("Go for it!");
 
   const createBoard = (word) => {
     let newBoard = [];
     for (let i = 0; i < word.length; i++) {
-      newBoard.push('_')
+      newBoard.push("_");
     }
-    setBoard(newBoard)
+    setBoard(newBoard);
     return newBoard;
-  }
+  };
 
   useEffect(() => {
-    createBoard(word)
-  }, [word])
+    createBoard(word);
+  }, [word]);
 
   useEffect(() => {
-    setHp(limit - tries)
-  }, [tries, limit])
+    setHp(limit - tries);
+  }, [tries, limit]);
 
   useEffect(() => {
-    if (board.indexOf('_') !== -1) {
-      return setCompleted(false)
+    if (board.indexOf("_") !== -1) {
+      return setCompleted(false);
     }
-    return setCompleted(true)
-  }, [board])
+    return setCompleted(true);
+  }, [board]);
 
   const checkLetter = (char) => {
-    if (word.split('').indexOf(char) > -1) {
+    if (word.split("").indexOf(char) > -1) {
       return true;
     }
     return false;
-  }
+  };
 
   const setCorrect = (char) => {
-    let newBoard = [...board]
-    word.split('').forEach((letter, i) => {
+    let newBoard = [...board];
+    word.split("").forEach((letter, i) => {
       if (char === letter) {
-        newBoard[i] = char
+        newBoard[i] = char;
       }
-    })
-    setBoard(newBoard)
-  }
+    });
+    setBoard(newBoard);
+  };
+
+  const checkError = (letter) => {
+    if (letter === "" || /\d/.test(letter)) {
+      setError("Submit a letter");
+      return true;
+    } else if (board.indexOf(letter) !== -1 || used.indexOf(letter) !== -1) {
+      setError("You already used this letter");
+      return true;
+    }
+    return false;
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const letter = e.target.letter.value;
-    e.target.letter.value = '';
+    const letter = e.target.letter.value.toUpperCase();
+    e.target.letter.value = "";
 
-    if (letter === '' || used.indexOf(letter) !== -1) {
-      return null
+    setError("");
+
+    if (checkError(letter)) {
+      return null;
     }
 
     if (checkLetter(letter)) {
-      setCorrect(letter)
+      setCorrect(letter);
+      setError("Correct!");
     } else {
-      setTries(tries + 1)
-      setUsed([...used, letter])
+      setTries(tries + 1);
+      setUsed([...used, letter]);
+      setError("Wrong!");
     }
-  }
-
-  const isCompleted = () => {
-    return board.infexOf('_') === -1
-  }
-
-  const renderResults = () => {
-    console.log(board.indexOf('_'));
-    if (board.indexOf('_') > -1) {
-      return <p>Keep playing!</p>
-    }
-    return <p>Congratulations!</p>
-  }
+  };
 
   const renderForm = () => {
     return (
-      <form
-      autoComplete='off'
-        onSubmit={handleSubmit}>
-        <input type="text" id="hm-match-letter" name="letter"></input>
-        <input type="submit"></input>
+      <form autoComplete="off" onSubmit={handleSubmit}>
+        <input type="text" id="hm-match-letter" name="letter" />
+        <br />
+        <input type="submit" />
       </form>
-    )
-  }
+    );
+  };
+
+  const restartGame = () => {
+    setReady(false);
+  };
 
   return (
     <div>
-      Hello from hangman match
       <div>
-        <p>Word is {word}</p>
-        {hp > 0 &&
+        {hp > 0 && (
           <div>
-            <p>Guess the word {board && board.join(' ')}</p>
-            <div>{!completed && <div>
-              <p>Type a letter</p>
-              {renderForm()}
-              <div>Lives: {hp}</div>
-              {used.length > 0 && <div>
-                <p>Wrong letters:</p>
-                <p>{used.join(',')}</p>
-              </div>}
-            </div>}</div>
+            <p>Adivina la palabra:</p>
+            <p style={{ fontSize: "30px" }}>{board && board.join(" ")}</p>
+            <p>
+              Pista: <strong>{hint}</strong>
+            </p>
+            <div>
+              {!completed && (
+                <div>
+                  <p>Escribe una letra:</p>
+                  {renderForm()}
+                  {error && <p>{error}</p>}
+                  <div>Lives: {hp}</div>
+                  {used.length > 0 && (
+                    <div>
+                      <p>Wrong letters:</p>
+                      <p>{used.join(",")}</p>
+                    </div>
+                  )}
+                </div>
+              )}
+            </div>
 
-            {completed &&
+            {completed && (
               <div>
-                <p>
-                  Congratulations!
-                </p>
-                {tries > 0 ? <p>
-                  You completed the word with <strong>{`${tries} strikes`}</strong>
-                </p>
-                  : <p>You completed the word with <strong>no strikes!</strong></p>}
-              </div>}
-          </div>}
-        {hp <= 0 &&
+                <p>Congratulations!</p>
+                {tries > 0 ? (
+                  <p>
+                    You completed the word with{" "}
+                    <strong>{`${tries} strikes`}</strong>
+                  </p>
+                ) : (
+                  <p>
+                    You completed the word with <strong>no strikes!</strong>
+                  </p>
+                )}
+                <button onClick={restartGame}>New Game</button>
+              </div>
+            )}
+          </div>
+        )}
+        {hp <= 0 && (
           <div>
             <p>You lost</p>
-            <p>The word was <strong>{word}</strong></p>
-          </div>}
+            <p>
+              The word was <strong>{word}</strong>
+            </p>
+            <button onClick={restartGame}>New Game</button>
+          </div>
+        )}
       </div>
     </div>
-  )
+  );
 }
 
 export default HangmanMatch;
--- src/pages/HangmanPage.js
+++ src/pages/HangmanPage.js
@@ -1,13 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import HangmanMatch from "../components/HangmanMatch";
 
 const HangmanPage = () => {
+  const [ready, setReady] = useState(false);
+  const [word, setWord] = useState("");
+  const [limit, setLimit] = useState("");
+  const [hint, setHint] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { word, limit, hint } = e.target;
+    setWord(word.value.toUpperCase());
+    setLimit(limit.value);
+    setHint(hint.value);
+    setReady(true);
+  };
+
   return (
     <div>
-      Hello from hangPage
-      <HangmanMatch word={'mamita'} limit={5}/>
+      Hello from hangPagedd
+      {!ready && (
+        <div>
+          <form onSubmit={handleSubmit}>
+            <label>
+              Word:
+              <input type="text" name="word" />
+            </label>
+            <label>
+              Lives:
+              <input type="number" name="limit" />
+            </label>
+            <label>
+              Hint:
+              <input type="text" name="hint" />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+      )}
+      {ready && (
+        <HangmanMatch
+          word={word}
+          limit={limit}
+          hint={hint}
+          setReady={setReady}
+        />
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default HangmanPage;
